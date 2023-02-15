28c6eef981396dcf8a9334ca592c11808773b2f7
twentyxe7en <89832715+twentyxe7en@users.noreply.github.com>
2023-02-15 23:26:21 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,53 +1,8 @@
 package atm;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        Process p = new Process();
-        Account acc = new Account();
-        Authentication auth = new Authentication();
-        
-        int looper = 1;
-        while (looper > 0) {
-            System.out.println("Please Log In");
-            System.out.print("ID: ");
-            int id = in.nextInt();
-            System.out.print("Key: ");
-            int key = in.nextInt();
-            while (auth.login(id, key, acc.getId(), acc.getKey())) {
-                System.out.println("======================");
-                System.out.println("[1] View Savings");
-                System.out.println("[2] Deposit to Savings");
-                System.out.println("[3] Withdraw Savings");
-                System.out.println("[0] Exit");
-                System.out.println("======================");
-                int action = in.nextInt();
-                switch (action) {
-                    case 1:
-                        System.out.println("View Savings");
-                        System.out.println("Your Savings: " + acc.getSavings());
-                        break;
-                    case 2:
-                        System.out.println("Deposit to Savings");
-                        System.out.print("Enter deposit amount: ");
-                        p.deposit(in.nextDouble(), id, key, acc.getId(), acc.getKey(), acc.getSavings());
-                        System.out.println("Your Savings: " + acc.getSavings());
-                        break;
-                    case 3:
-                        System.out.println("Withdraw from Savings");
-                        System.out.print("Enter withdraw amount: ");
-                        p.withdraw(in.nextDouble(), id, key, acc.getId(), acc.getKey(), acc.getSavings());
-                        System.out.println("Your Savings: " + acc.getSavings());
-                        break;
-                    case 0:
-                        System.out.println("Exiting program.");
-                        System.exit(0);
-                        break;
-                    default:
-                        looper++;
-                }
-            }
-        }
+        EventController event = new EventController();
+        event.playEvent();
     }
 }
