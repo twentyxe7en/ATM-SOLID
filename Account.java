5fbacb19afba796dda16330c950163a28c769624
twentyxe7en <89832715+twentyxe7en@users.noreply.github.com>
2023-02-15 23:11:57 +0800

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,39 +1,51 @@
 package atm;
+
+import java.util.ArrayList;
+
 public class Account {
 
-    public Account(int id, int key) {
-        this.id = id;
-        this.key = key;
-        this.savings = 10000;
+    public Account() {
+        id.add(1234);
+        key.add(5678);
+        savings.add(10000.00);
     }
     
-    private int id;
-    private int key;
-    private double savings;
+    ArrayList<Integer> id = new ArrayList<>();
+    ArrayList<Integer> key = new ArrayList<>();
+    ArrayList<Double> savings = new ArrayList<>();
 
-    public int getId() {
+    public ArrayList<Integer> getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getKey() {
+    public ArrayList<Integer> getKey() {
         return key;
     }
 
-    public void setKey(int key) {
-        this.key = key;
-    }
-
-    public double getSavings() {
+    public ArrayList<Double> getSavings() {
         return savings;
     }
 
-    public void setSavings(double savings) {
+    public void setSavings(ArrayList<Double> savings) {
         this.savings = savings;
     }
     
+    
+    
+    public void addAccount(Integer id, Integer key) {
+        this.id.add(id);
+        this.key.add(key);
+        savings.add(10000.00);
+    }
+    
+    public void removeAccount(Integer id, Integer key) {
+        for (int i = 0; i < this.id.size(); i++) {
+            if (this.id.get(i) == id && this.key.get(i) == key) {
+                this.id.remove(i);
+                this.key.remove(i);
+                savings.remove(i);
+            }
+        }
+    }
     
 }
