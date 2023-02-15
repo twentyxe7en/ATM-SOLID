d9e1da185b88ea5a0ebfb464c7e6ee2f74e09a3d
twentyxe7en <89832715+twentyxe7en@users.noreply.github.com>
2023-02-15 23:11:43 +0800

    Update Process.java

--- Process.java
+++ Process.java
@@ -1,19 +1,37 @@
 package atm;
+
+import java.util.ArrayList;
+
 public class Process {
 
     public Process() {
     }
     
-    public double deposit(double in, double sav) {
-        
-        return in + sav;
+    public double deposit(double in, int eid, int ekey, ArrayList<Integer> id, ArrayList<Integer> key, ArrayList<Double> sav) {
+        double dep = 0;
+        for (int i = 0; i < id.size(); i++) {
+            if (id.get(i) == eid && key.get(i) == ekey) {
+                sav.set(i, sav.get(i) + in);
+            }
+            dep = sav.get(i);
+        }
+        return dep;
     }
     
-    public double withdraw(double out, double sav) {
-        if (sav > out)
-            return sav - out;
-        else
-            System.out.println("You have insufficient funds.");
-        return sav;
+    public double withdraw(double out, int eid, int ekey, ArrayList<Integer> id, ArrayList<Integer> key, ArrayList<Double> sav) {
+        double wd = 0;
+        
+        for (int i = 0; i < id.size(); i++) {
+            if (id.get(i) == eid && key.get(i) == ekey) {
+                if (sav.get(i) > out) {
+                    sav.set(i, sav.get(i) - out);
+                } else {
+                    System.out.println("Insufficient savings.");
+                }
+                wd = sav.get(i);
+            }
+        }
+        
+        return wd;
     }
 }
